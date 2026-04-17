ee152125167c28244e75a0b74c1f85a329a1205e
manojLovesCoding <mkum9417@gmail.com>
2026-04-17 11:08:25 +0530

    chore(config): switch API and file URLs to production domain

--- frontend/src/components/common/http.jsx
+++ frontend/src/components/common/http.jsx
@@ -1,5 +1,8 @@
-export const apiUrl = 'http://localhost:8000/api/'
-export const fileUrl = 'http://localhost:8000/'
+//export const apiUrl = 'http://localhost:8000/api/'
+//export const fileUrl = 'http://localhost:8000/'
+
+export const apiUrl = 'https://vayuhu.fun/api/'
+export const fileUrl = 'https://vayuhu.fun/'
 
 export const token = () => {
     const userInfo = localStorage.getItem('userInfo')
